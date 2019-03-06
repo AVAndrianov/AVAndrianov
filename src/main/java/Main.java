5da87fd57a440355a16d5f056c081bcf17aec43a
AVAndrianov <a.v.andrianov33@gmail.com>
2019-03-06 16:38:46 +0300

    gg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("Hello Anton");
         //????
         System.out.println("Hello Vasya");
+        System.out.println("Hello Grigoriy");
     }
 
 }
