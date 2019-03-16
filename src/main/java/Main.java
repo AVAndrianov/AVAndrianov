00cac589d0dd543b0523d352b5c50b7551a8d83d
AVAndrianov <34946251+AVAndrianov@users.noreply.github.com>
2019-03-16 03:07:35 +0300

    Update Main.java
    saut

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,11 @@ public class Main {
         System.out.println("Hello Anton");
         System.out.println("Hello Anton");
         System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
     }
 
 }
