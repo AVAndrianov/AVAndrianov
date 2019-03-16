f67eb6701c7d37de6e56777db94556d51df79134
AVAndrianov <a.v.andrianov33@gmail.com>
2019-03-16 03:17:25 +0300

    Hello World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,36 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //LLL
-        //LLL
-        //LLL
-        //LLL
-        //LLL
-       C
+
+        MainClassTask2 task2 = new MainClassTask2();
+//        MainClassTask1 task1 = new MainClassTask1();
+        System.out.println("Hello Anton");
+        //????
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        System.out.println("Hello Anton");
+        MainClassTask2 task2 = new MainClassTask2();
+
+        MainClassTask2 task2 = new MainClassTask2();
+
+        MainClassTask2 task2 = new MainClassTask2();
+
+        MainClassTask2 task2 = new MainClassTask2();
+
+        MainClassTask2 task2 = new MainClassTask2();
+        MainClassTask2 task2 = new MainClassTask2();
+        MainClassTask2 task2 = new MainClassTask2();
+
     }
 
 }
