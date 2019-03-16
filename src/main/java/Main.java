cc3ba4b3fd4265a5203738daccd3044e837dd5e8
AVAndrianov <34946251+AVAndrianov@users.noreply.github.com>
2019-03-16 03:22:41 +0300

    Update Main.java
    Git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,34 +5,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        MainClassTask2 task2 = new MainClassTask2();
-//        MainClassTask1 task1 = new MainClassTask1();
-        System.out.println("Hello Anton");
-        //????
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        //        MainClassTask1 task1 = new MainClassTask1();
-        //        MainClassTask1 task1 = new MainClassTask1();
-        //        MainClassTask1 task1 = new MainClassTask1();
-        //        MainClassTask1 task1 = new MainClassTask1();
-        //        MainClassTask1 task1 = new MainClassTask1();
-        
-         System.out.println("Hello Anton");
-        System.out.println("Hello Anton");
-        //        MainClassTask1 task1 = new MainClassTask1();
-        //        MainClassTask1 task1 = new MainClassTask1();
+        //Git
+        //Git
+        //Git
     }
 
 }
